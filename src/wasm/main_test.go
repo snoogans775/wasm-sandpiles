a21c5c4329793e692bc919c710cd712e5d772274
Kevin Fredericks <snoogans775@gmail.com>
2022-09-16 08:04:25 -0700

    remove outdated test

--- src/wasm/main_test.go
+++ src/wasm/main_test.go
@@ -6,11 +6,5 @@ import (
 )
 
 func testInitSandpile(t *testing.T) {
-  newPile = initSandpiles(100, 1000)
-  type = reflect.TypeOf(newPile).String()
-  targetType := "int[][]"  
-
-  if  type != targetType {
-    t.Errorf("got %q, wanted %q", type, targetType) 
-  } 
+  }
 }
